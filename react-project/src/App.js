c3a7c5762e4c02c78c85a2ac55811e01c77b68e9
Mitesh Makwana <mitesh.makwana@exhibytesolution.com>
2022-05-05 11:18:44 +0530

    removed styled compnents from app.js

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
-
+import styled from "styled-components";
 import React, { Component } from "react";
 import Person from "./Person/Person";
+
+const StyledButton = styled.button`
+  background-color: ${(props) => (props.alt ? "red" : "green")};
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${(props) => (props.alt ? "salmon" : "lightgreen")};
+    color: black;
+  }
+`;
 
 class App extends Component {
   state = {
@@ -93,17 +107,17 @@ class App extends Component {
     }
 
     return (
-      <styleRoot>
-        <div className="App">
-          <h1>Hello</h1>
-          <p className={classes.join(" ")}> This is really working here</p>
-          <button className="button" onClick={this.togglePersonsHandler}>
-            {" "}
-            Toggle Persons{" "}
-          </button>
-          {persons}
-        </div>
-      </styleRoot>
+      <div className="App">
+        <h1>Hello</h1>
+        <p className={classes.join(" ")}> This is really working here</p>
+        <StyledButton
+          alt={this.state.showPersons}
+          onClick={this.togglePersonsHandler}
+        >
+          Toggle Persons
+        </StyledButton>
+        {persons}
+      </div>
     );
   }
   // return React.createElement(
